c4f5813b686c7824b5d1df029ab009c1ccececd7
Fabiano Weimar <xiru@xiru.org>
2024-01-14 13:35:28 +0000

    Refactory as a class with internal state.

--- nvidia_fan_controller.py
+++ nvidia_fan_controller.py
@@ -10,41 +10,93 @@ from time import sleep
 logger = logging.getLogger('nvidia-fan-controller')
 
 
-def run_cmd(cmd):
-    logger.debug("Running cmd: %s", ' '.join(cmd))
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
+class NVidiaFanController(object):
 
-    if p.returncode:
-        logger.critical("Unable to run cmd: %s", ' '.join(cmd))
-        if p.stderr is not None:
-            for line_bytes in p.stderr.readlines():
-                line = line_bytes.decode().strip()
-                if line:
-                    logger.error("Caught process stderr: %s", line)
-        raise subprocess.CalledProcessError(p.returncode, cmd)
+    def __init__(self, interval_secs=2):
+        self.fan_control = False
+        self.interval_secs = interval_secs
+        self.lookup()
 
-    return '' if p.stdout is None else p.stdout.read().decode()
+    def _run_cmd(self, cmd):
+        logger.debug("Running cmd: %s", ' '.join(cmd))
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+
+        if p.returncode:
+            logger.critical("Unable to run cmd: %s", ' '.join(cmd))
+            if p.stderr is not None:
+                for line_bytes in p.stderr.readlines():
+                    line = line_bytes.decode().strip()
+                    if line:
+                        logger.error("Caught process stderr: %s", line)
+            raise subprocess.CalledProcessError(p.returncode, cmd)
+
+        return '' if p.stdout is None else p.stdout.read().decode()
+
+    def get_measurements(self):
+        ''' retrieve metrics using nvidia-smi as [(index, temperature, fan_speed, utilization)]
+        '''
+        stdout = self._run_cmd(['nvidia-smi', '--query-gpu=index,temperature.gpu,fan.speed,utilization.gpu', '--format=csv,noheader'])
+        measurements = [tuple(map(int, values)) for values in re.findall(r'(\d+), (\d+), (\d+) %, (\d+) %', stdout, flags=re.MULTILINE)]
+        if not measurements:
+            raise RuntimeError("no gpu detected")
+        return measurements
+
+    def lookup(self):
+        self.measurements = self.get_measurements()
+
+    def disable_manual_gpu_fan_control(self):
+        if self.fan_control:
+            logger.debug("Disabling manual gpu fan control")
+            self._run_cmd(['nvidia-settings', '--assign', 'GPUFanControlState=0'])
+            self.fan_control = False
+
+    def set_fan_speed(self, index, fan_speed):
+        logger.info("Setting new fan speed %s on gpu %d", fan_speed, index)
+        config = f'[fan-{index:d}]/GPUTargetFanSpeed={fan_speed:d}'
+        self._run_cmd(['nvidia-settings', '--assign', 'GPUFanControlState=1', '--assign', config])
+        self.fan_control = True
+
+    def idle(self):
+        ''' check if all gpus are idle
+        '''
+        for _, temperature, _, utilization in self.measurements:
+            if temperature > 40 or utilization > 10:
+                return False
+        return True
+
+    def _run(self):
+        while True:
+            if self.idle():
+                self.disable_manual_gpu_fan_control()
+            else:
+                for index, temperature, fan_speed, _ in self.measurements:
+                    # predictable (hardcoded) fan speed ramp
+                    if temperature > 60:
+                        new_fan_speed = 100
+                    elif temperature > 55:
+                        new_fan_speed = 90
+                    elif temperature > 50:
+                        new_fan_speed = 75
+                    elif temperature > 45:
+                        new_fan_speed = 60
+                    else:
+                        new_fan_speed = 30
+                    if abs(new_fan_speed - fan_speed) > 2:
+                        self.set_fan_speed(index, new_fan_speed)
+            sleep(self.interval_secs)
+            self.lookup()
+
+    def run(self):
+        try:
+            self._run()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.disable_manual_gpu_fan_control()
 
 
-def get_measurements():
-    stdout = run_cmd(['nvidia-smi', '--query-gpu=index,temperature.gpu,fan.speed,utilization.gpu', '--format=csv,noheader'])
-    measurements = re.findall(r'(\d+), (\d+), (\d+) %, (\d+) %', stdout, flags=re.MULTILINE)
-    return [tuple(map(int, values)) for values in measurements]   # [(index, temperature, fan_speed, utilization)]
-
-
-def disable_manual_gpu_fan_control():
-    logger.debug("disabling manual gpu fan control")
-    run_cmd(['nvidia-settings', '--assign', 'GPUFanControlState=0'])
-
-
-def set_fan_speed(index, fan_speed):
-    config = f'[fan-{index:d}]/GPUTargetFanSpeed={fan_speed:d}'
-    logger.info("Setting new fan speed: %s", config)
-    run_cmd(['nvidia-settings', '--assign', 'GPUFanControlState=1', '--assign', config])
-
-
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--interval-secs', type=int, default=5,
                         help="number of seconds between consecutive updates")
@@ -54,47 +106,4 @@ def main():
 
     logging.basicConfig(level=getattr(logging, args.log_level))
 
-    measurements = get_measurements()
-
-    if not measurements:
-        raise RuntimeError("no gpu detected")
-
-    while True:
-
-        # check if all gpus are idle
-        idle = True
-        for _, temperature, _, utilization in measurements:
-            if temperature > 40 or utilization > 10:
-                idle = False
-                break
-        if idle:
-            disable_manual_gpu_fan_control()
-        else:
-
-            for index, temperature, fan_speed, _ in measurements:
-
-                # predictable (hardcoded) fan speed ramp
-                if temperature > 60:
-                    new_fan_speed = 100
-                elif temperature > 55:
-                    new_fan_speed = 90
-                elif temperature > 50:
-                    new_fan_speed = 75
-                elif temperature > 45:
-                    new_fan_speed = 60
-                else:
-                    new_fan_speed = 30
-
-                if abs(new_fan_speed - fan_speed) > 2:
-                    set_fan_speed(index, new_fan_speed)
-
-        sleep(args.interval_secs)
-
-        measurements = get_measurements()
-
-
-if __name__ == '__main__':
-    try:
-        main()
-    finally:
-        disable_manual_gpu_fan_control()
+    NVidiaFanController(args.interval_secs).run()
